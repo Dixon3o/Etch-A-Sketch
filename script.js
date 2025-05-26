7fcaf454e7b2517d353d916166aabd2958927886
Dixon3o <dimitrije.bibic64@gmail.com>
2025-05-26 11:48:37 +0200

    Make grid

---
+++ script.js
@@ -0,0 +1,12 @@
+const cdiv = document.querySelector(".container");
+
+function makeGrid(size){
+    for(let i = 0; i<size; i++){
+        for(let j = 0; j<size; j++){
+            let div = document.createElement('div');
+            div.classList.add('cell');
+            cdiv.appendChild(div);
+        }
+    }
+}
+makeGrid(16);
